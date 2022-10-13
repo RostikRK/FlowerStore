2f2c8dffcc9ddfd851cdfd4156cd4e8506fb1536
Rostik <rostyslav.kostiuk@ucu,edu>
2022-10-14 00:13:34 +0300

    Fixed style

--- src/main/java/flower/store/Rose.java
+++ src/main/java/flower/store/Rose.java
@@ -1,6 +1,6 @@
 package flowerstore.src.main.java.flower.store;
 
-public class Rose extends Flower{
+public class Rose extends Flower {
     public Rose() {
         super(2, FlowerColor.RED, 0, FlowerType.ROSE);
     }
